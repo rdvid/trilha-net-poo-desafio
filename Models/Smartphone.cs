cbf32100fb91a96ddfc188807031bd0e280ba27c
Rafael <rafael_dvid@hotmail.com>
2024-01-12 15:28:52 -0300

    sobrescrevendo metodo de instalar app das classes Nokia e Iphone + adicionando construtor a classe Smartphone

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,10 +4,16 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
+        protected string Modelo;
+        protected string IMEI;
+        protected int Memoria;
+        
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
             // TODO: Passar os parâmetros do construtor para as propriedades
         }
 
--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Iphone:");
+Smartphone iphone = new Iphone("987654", "Modelo 2", "456789", 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Kindle");
+
+Console.WriteLine("Nokia:");
+Smartphone nokia = new Nokia("654321", "Modelo 1", "123456", 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Google Maps");
+
